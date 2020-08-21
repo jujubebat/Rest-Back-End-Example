c788644e9711fd4754e78fee00e0c3ee516585c1
jujubebat <bbwwpark@naver.com>
2020-08-21 23:10:58 +0900

    S

--- src/main/java/shop/gongdal/controller/api/ProductApiController.java
+++ src/main/java/shop/gongdal/controller/api/ProductApiController.java
@@ -7,6 +7,7 @@ import shop.gongdal.payload.ProductRequest;
 import shop.gongdal.service.ProductService;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -27,44 +28,56 @@ public class ProductApiController {
 
     // 공매물건 조건에 따른검색 컨트롤러
     @GetMapping(path = "/search")
-    List<Product> getProductByCarCondition(@RequestParam(required = true) String pageStartNum,
+    List<Product> getProductByCarCondition(@RequestParam(required = true) Optional<Long> pageStartNum,
                                            @RequestParam(required = false) String objectManagementNum,
                                            @RequestParam(required = false) String objectName,
-                                           @RequestParam(required = false) String appraisedPriceStart,
-                                           @RequestParam(required = false) String appraisedPriceEnd,
-                                           @RequestParam(required = false) String bidBeginDateTime,
-                                           @RequestParam(required = false) String bidCloseDateTime,
+                                           @RequestParam(required = false) Optional<Long> appraisedPriceStart,
+                                           @RequestParam(required = false) Optional<Long> appraisedPriceEnd,
+                                           @RequestParam(required = false) Optional<Long> bidBeginDateTime,
+                                           @RequestParam(required = false) Optional<Long> bidCloseDateTime,
                                            @RequestParam(required = false) String manufacturer,
                                            @RequestParam(required = false) String model,
                                            @RequestParam(required = false) String transmission,
                                            @RequestParam(required = false) String fuelType,
-                                           @RequestParam(required = false) String yearAndMonthStart,
-                                           @RequestParam(required = false) String yearAndMonthEnd,
-                                           @RequestParam(required = false) String displacementStart,
-                                           @RequestParam(required = false) String displacementEnd,
-                                           @RequestParam(required = false) String distanceDrivenStart,
-                                           @RequestParam(required = false) String distanceDrivenEnd) {
+                                           @RequestParam(required = false) Optional<Long> yearAndMonthStart,
+                                           @RequestParam(required = false) Optional<Long> yearAndMonthEnd,
+                                           @RequestParam(required = false) Optional<Long> displacementStart,
+                                           @RequestParam(required = false) Optional<Long> displacementEnd,
+                                           @RequestParam(required = false) Optional<Long> distanceDrivenStart,
+                                           @RequestParam(required = false) Optional<Long> distanceDrivenEnd) {
 
+        System.out.println("시바 "+ pageStartNum);
 
         ProductRequest productRequest = new ProductRequest();
 
-        productRequest.setPageStartNum(Long.parseLong(pageStartNum));
+        if(pageStartNum.isPresent())
+            productRequest.setPageStartNum((Long)pageStartNum.get());
         productRequest.setObjectManagementNum(objectManagementNum);
         productRequest.setObjectName(objectName);
-        productRequest.setAppraisedPriceStart(Long.parseLong(appraisedPriceStart));
-        productRequest.setAppraisedPriceEnd(Long.parseLong(appraisedPriceEnd));
-        productRequest.setBidBeginDateTime(Long.parseLong(bidBeginDateTime));
-        productRequest.setBidCloseDateTime(Long.parseLong(bidCloseDateTime));
+        if(appraisedPriceStart.isPresent())
+            productRequest.setAppraisedPriceStart((Long)appraisedPriceStart.get());
+        if(appraisedPriceEnd.isPresent())
+            productRequest.setAppraisedPriceEnd((Long)appraisedPriceEnd.get());
+        if(bidBeginDateTime.isPresent())
+            productRequest.setBidBeginDateTime((Long)bidBeginDateTime.get());
+        if(bidCloseDateTime.isPresent())
+            productRequest.setBidCloseDateTime((Long)bidCloseDateTime.get());
         productRequest.setManufacturer(manufacturer);
         productRequest.setModel(model);
         productRequest.setTransmission(transmission);
         productRequest.setFuelType(fuelType);
-        productRequest.setYearAndMonthStart(Long.parseLong(yearAndMonthStart));
-        productRequest.setYearAndMonthEnd(Long.parseLong(yearAndMonthEnd));
-        productRequest.setDisplacementStart(Long.parseLong(displacementStart));
-        productRequest.setDistanceDrivenEnd(Long.parseLong(displacementEnd));
-        productRequest.setDistanceDrivenStart(Long.parseLong(distanceDrivenStart));
-        productRequest.setDistanceDrivenEnd(Long.parseLong(distanceDrivenEnd));
+        if(yearAndMonthStart.isPresent())
+            productRequest.setYearAndMonthStart((Long)yearAndMonthStart.get());
+        if(yearAndMonthEnd.isPresent())
+            productRequest.setYearAndMonthEnd((Long)yearAndMonthEnd.get());
+        if(displacementStart.isPresent())
+            productRequest.setDisplacementStart((Long)displacementStart.get());
+        if(displacementEnd.isPresent())
+            productRequest.setDistanceDrivenEnd((Long)displacementEnd.get());
+        if(displacementStart.isPresent())
+            productRequest.setDistanceDrivenStart((Long)distanceDrivenStart.get());
+        if(displacementEnd.isPresent())
+            productRequest.setDistanceDrivenEnd((Long)distanceDrivenEnd.get());
 
         return productService.getProductByCondition(productRequest);
     }
